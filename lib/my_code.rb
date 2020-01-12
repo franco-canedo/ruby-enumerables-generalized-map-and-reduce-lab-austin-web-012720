60d82bf1ba79c69ba15f92e3a7624ebcb1991a07
Franco Canedo <francanedo1@hotmail.com>
2020-01-12 22:54:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,11 +10,5 @@ def map(array)
 end
 
 def reduce(source_array=0)
-  new_array = []
-  i = 0 
-  while i < array.length do 
-   new_array << yield(array[i])
-   i += 1
-  end 
-  new_array
+ 
 end 
